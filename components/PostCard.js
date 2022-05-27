319d4ec71a9a44a17318b00fdae351999dc06b4c
wkhmarkwong <75322185+wkhmarkwong@users.noreply.github.com>
2022-05-27 22:49:50 +0800

    add unpublish function

--- components/PostCard.js
+++ components/PostCard.js
@@ -2,70 +2,84 @@ import { useState } from 'react';
 import { useRouter } from 'next/router';
 
 export default function PostCard({ post }) {
-    const [publishing, setPublishing] = useState(false);
-    const [deleting, setDeleting] = useState(false);
-    const router = useRouter();
+  const [publishing, setPublishing] = useState(false);
+  const [deleting, setDeleting] = useState(false);
+  const router = useRouter();
 
-    // Publish post
-    const publishPost = async (postId) => {
-        // change publishing state
-        setPublishing(true);
+  // Publish post
+  const publishPost = async (postId, postPublished) => {
+    // change publishing state
+    setPublishing(true);
 
-        try {
-            // Update post
-            await fetch('/api/posts', {
-                method: 'PUT',
-                body: postId,
-            });
+    console.log(postPublished);
 
-            // reset the publishing state
-            setPublishing(false);
+    let passingData = { postId: postId, postPublished: postPublished };
 
-            // reload the page
-            return router.push(router.asPath);
-        } catch (error) {
-            // Stop publishing state
-            return setPublishing(false);
-        }
-    };
-    // Delete post
-    const deletePost = async (postId) => {
-        //change deleting state
-        setDeleting(true);
+    try {
+      // Update post
+      await fetch('/api/posts', {
+        method: 'PUT',
+        body: JSON.stringify(passingData),
+      });
 
-        try {
-            // Delete post
-            await fetch('/api/posts', {
-                method: 'DELETE',
-                body: postId,
-            });
+      // reset the publishing state
+      setPublishing(false);
 
-            // reset the deleting state
-            setDeleting(false);
+      // reload the page
+      return router.push(router.asPath);
+    } catch (error) {
+      // Stop publishing state
+      return setPublishing(false);
+    }
+  };
+  // Delete post
+  const deletePost = async (postId) => {
+    //change deleting state
+    setDeleting(true);
 
-            // reload the page
-            return router.push(router.asPath);
-        } catch (error) {
-            // stop deleting state
-            return setDeleting(false);
-        }
-    };
-    return (
-        <>
-            <li>
-                <h3>{post.title}</h3>
-                <p>{post.content}</p>
-                <small>{new Date(post.createdAt).toLocaleDateString()}</small>
-                <br />
-                {!post.published ? (
-                    <button type="button" onClick={() => publishPost(post._id)}>
-                        {publishing ? 'Publishing' : 'Publish'}
-                    </button>
-                ) : null}
-                <button type="button" onClick={() => deletePost(post['_id'])}>
-                    {deleting ? 'Deleting' : 'Delete'}
-                </button>
-            </li>
-        </>
-    );
+    try {
+      // Delete post
+      await fetch('/api/posts', {
+        method: 'DELETE',
+        body: postId,
+      });
+
+      // reset the deleting state
+      setDeleting(false);
+
+      // reload the page
+      return router.push(router.asPath);
+    } catch (error) {
+      // stop deleting state
+      return setDeleting(false);
+    }
+  };
+  return (
+    <>
+      <li>
+        <h3>{post.title}</h3>
+        <p>{post.content}</p>
+        <small>{new Date(post.createdAt).toLocaleDateString()}</small>
+        <br />
+        {!post.published ? (
+          <button
+            type="button"
+            onClick={() => publishPost(post._id, post.published)}
+          >
+            {publishing ? 'Publishing' : 'Publish'}
+          </button>
+        ) : (
+          <button
+            type="button"
+            onClick={() => publishPost(post._id, post.published)}
+          >
+            Unpublish
+          </button>
+        )}
+        <button type="button" onClick={() => deletePost(post['_id'])}>
+          {deleting ? 'Deleting' : 'Delete'}
+        </button>
+      </li>
+    </>
+  );
 }
